9ae8e0d8932f3cad7404928e01ca8c27a3e9a262
Amir <abu.amir@mail.ru>
2020-10-29 01:05:22 +0300

    update

--- Hepergraph/Hypergraph.cs
+++ Hepergraph/Hypergraph.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 public class Hypergraph<T>
 {
@@ -129,7 +130,146 @@ public class Hypergraph<T>
 
         return true;
     }
+
+    //Is graph l-divided
+    public bool IsLDivided(int l)
+    {
+        HashSet<Vertex<T>> verticesInEdges = new HashSet<Vertex<T>>();
+
+        if (l != Edges.Count)
+            return false;
+        
+        foreach (var edge in Edges)
+        {
+            foreach (var vertex in edge.Vertices)
+            {
+                if (verticesInEdges.Contains(vertex))
+                    return false;
+                verticesInEdges.Add(vertex);
+            }
+        }
+
+        return true;
+    }
+
+    //Is graph L Homogeneous
+    public bool IsLHomogeneous(int l)
+    {
+        if (Edges.Count != l)
+            return false;
+
+        foreach (var edge in Edges)
+        {
+            foreach (var edge_other in Edges)
+            {
+                if(edge == edge_other)
+                    continue;
+                
+                if (IsEven(edge, edge_other))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsCombination(IEnumerable<Edge<T>> edges)
+    {
+        var edgesArray = edges as Edge<T>[] ?? edges.ToArray();
+        
+        foreach (var edge_1 in edgesArray)
+        {
+            foreach (var edge_2 in edgesArray)
+            {
+                if(edge_1 == edge_2)
+                    continue;
+
+                if (edge_1.Vertices.Intersect(edge_2.Vertices).Any())
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsPerfectCombination(IEnumerable<Edge<T>> edges)
+    {
+        IEnumerable<Edge<T>> enumerableEdges = edges as Edge<T>[] ?? edges.ToArray();
+        
+        if (!IsCombination(enumerableEdges))
+            return false;
+     
+        HashSet<Vertex<T>> vertices = new HashSet<Vertex<T>>();
+
+
+        foreach (var edge in enumerableEdges)
+        {
+            foreach (var vertex in edge.Vertices)
+            {
+                if (vertices.Contains(vertex))
+                    return false;
+
+                vertices.Add(vertex);
+            }
+        }
+
+        return true;
+    }
     
+    public bool IsLDividedHomogeneous(int l)
+    {
+        return IsLDivided(l) && IsLHomogeneous(l);
+    }
+
+    public bool IsStar(List<Edge<T>> edges, out int power)
+    {
+        power = -1;
+        
+        int count = 0;
+        HashSet<Vertex<T>> vertices = new HashSet<Vertex<T>>();
+
+        foreach (var edge in edges)
+        {
+            foreach (var vertex in edge.Vertices)
+            {
+                if (vertices.Contains(vertex))
+                    count++;
+
+                vertices.Add(vertex);
+            }
+        }
+
+        if (count > 1)
+            return false;
+
+        power = count;
+
+        return true;
+    }
+
+
+    public bool IsStarsCovered(List<List<Edge<T>>> edgesList)
+    {
+        List<Vertex<T>> vertices = new List<Vertex<T>>(Vertices); 
+        
+        foreach (var edges in edgesList)
+        {
+            if (!IsStar(edges, out _))
+                return false;
+
+            foreach (var edge in edges)
+            {
+                foreach (var vertex in edge.Vertices)
+                {
+                    if (vertices.Contains(vertex))
+                        vertices.Remove(vertex);
+                }
+            }
+        }
+
+        return !vertices.Any();
+    }
+
     //Get all event edges
     public Edge<T>[] GetEvenEdges(){
 
@@ -153,7 +293,7 @@ public class Hypergraph<T>
         return result.ToArray();
     }
 
-
+    
     //Visulize graph
     public override string ToString()
     {
@@ -210,10 +350,6 @@ public class Hypergraph<T>
 
         return result.ToArray();
     }
-    
-    
-
-    
 }
 
 public class Vertex<T>
